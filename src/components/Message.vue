ab08d0d5ec783e51c52952be521fe2bc9c596995
nickzmra <nzamora9@stu.southtexascollege.edu>
2021-04-21 18:13:05 -0500

    Message Layout

---
+++ src/components/Message.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container mx-auto flex bg-indigo-500 my-4 rounded-full">
+        <div>
+            <mdi:account class="bg-white rounded-full h-12 w-12 px-1"/>
+        </div>
+        <div class="flex flex-grow">
+            <div class="flex flex-grow items-center justify-between mx-4 font-thin text-xl text-white">
+                <p>Message Goes Here</p>
+                <p class="text-sm italic">Author Goes Here</p>
+            </div>
+        </div>
+    </div>
+</template>
