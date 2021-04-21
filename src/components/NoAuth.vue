dbc0cda8b091acd425fee09bb97e890b2ae0f6cf
nickzmra <nzamora9@stu.southtexascollege.edu>
2021-04-21 17:29:23 -0500

    Created home page for none authorized users

---
+++ src/components/NoAuth.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex flex-col justify-center items-center space-y-8">
+        <wpf:key-security class="w-48 h-48 mt-48 text-yellow-500" />
+        <h1 class="text-coolGray-300 text-6xl font-thin">Please Login to Continue</h1>
+    </div>
+</template>
