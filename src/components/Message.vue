c0e23726fdb64b58a6b6d40746df4656b88383be
nickzmra <nzamora9@stu.southtexascollege.edu>
2021-04-22 15:41:45 -0500

    Added Message functionality

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,13 +1,36 @@
 <template>
-    <div class="container mx-auto flex bg-blueGray-500 bg-opacity-50 my-4 rounded-full">
+    <div class="container mx-auto flex bg-opacity-50 my-4 rounded-full" :class="isUser ? 'bg-blueGray-500' :'bg-gray-500'">
         <div>
-            <mdi:account class="bg-white rounded-full h-12 w-12 px-1"/>
+            <!--<mdi:account class="bg-white rounded-full h-12 w-12 px-1"/>-->
+            <img class="rounded-full h-12" :src="message.userPhotoURL" :alt="`Avatar of ${message.userName}`"/>
         </div>
         <div class="flex flex-grow">
             <div class="flex flex-grow items-center justify-between mx-4 font-thin text-xl text-white">
-                <p>Message Goes Here</p>
-                <p class="text-sm italic">Author Goes Here</p>
+                <p>{{message.text}}</p>
+                <p class="text-sm italic">{{message.userName}}</p>
             </div>
         </div>
     </div>
 </template>
+
+<script setup>
+    import {defineProps, computed, ref} from 'vue'
+    import {authentication} from '~/helpers/useFirebase'
+
+    const {user} = authentication()
+
+    const isUser = computed(() => user.value.uid === props.message.userId)
+
+    const props = defineProps({
+        message: {
+            type: Object,
+            default: () => ({
+                userName: '',
+                userId: '',
+                userPhotoURL: '',
+                text: '',
+                createdAt: '',
+            }),
+        },
+    })
+</script>
--- src/helpers/useFirebase.js
+++ src/helpers/useFirebase.js
@@ -1,13 +1,17 @@
+import { ref, onUnmounted } from "vue"
 import firebase from "firebase/app"
 import { useAuth } from "@vueuse/firebase"
 import "firebase/auth"
+import "firebase/firestore"
 
 import { firebaseConfig } from "~/config/firebase"
+import { useIdle } from "@vueuse/core"
 
 firebase.initializeApp(firebaseConfig)
 
-const { auth } = firebase
+const { auth, firestore } = firebase
 const { GoogleAuthProvider } = auth
+const db = firestore()
 
 const { isAuthenticated, user } = useAuth()
 
@@ -17,4 +21,31 @@ export const authentication = () => {
   return { googlePopup, signOut, isAuthenticated, user }
 }
 
-export const database = () => {}
+export const database = () => {
+  const messages = ref([])
+
+  const messageCollection = db.collection("messages")
+  const messageQuery = messageCollection.orderBy("createdAt", "desc").limit(100)
+
+  const unsubscribe = messageQuery.onSnapshot((s) => {
+    messages.value = s.docs
+      .map((doc) => ({ id: doc.id, ...doc.data() }))
+      .reverse()
+  })
+
+  onUnmounted(unsubscribe)
+
+  const sendMessage = (text) => {
+    if (!isAuthenticated.value) return
+    const { photoURL, uid, displayName } = user.value
+    messageCollection.add({
+      userName: displayName,
+      userId: uid,
+      userPhotoURL: photoURL,
+      text,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    })
+  }
+
+  return { messages, sendMessage }
+}
